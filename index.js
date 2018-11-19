1a8bf365b35a9edca0649e47ee1031199e912839
TheIceMagmaCube <38074756+TheIceMagmaCube@users.noreply.github.com>
2018-11-19 17:48:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //Initisation
 const Discord = require('discord.js');
 const bot = new Discord.Client()
-const cfg = require('./index.json');
 const token = process.env.token;
 
 //Message de jeu dans le status
