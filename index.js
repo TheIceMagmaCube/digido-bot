2f600ea788173f6fcb42c868536782858185238e
Damax41 <45049252+Damax41@users.noreply.github.com>
2018-11-23 21:45:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ client.on("message", async message => {
 	if(command === "help") {
 		message.channel.send({embed: {
 			color: 130,
-			description: `**Liste des commandes :**\n\n\`//help\`\n *Utilisation: //help*\n\n\`//ping\`\n *Utilisation: //ping*\n\n\`//say\`\n *Utilisation: //say La chose à faire dire au bot !*\n\n\`//poll\`\n *Utilisation : //poll |Titre du sondage|Proposition 1|Proposition 2|Proposition 3|Proposition 4*\n\n\`//kick\`\n *Utilisation: //kick @lenomdumembre#0000 La raison du kick !*\n\n\`//ban\`\n *Utilisation: //ban @lenomdumembre#0000 La raison du ban !*\n\n\`//nuke\`\n *Utilisation: //nuke Un_nombre_entre_2_et_100*\n\n\`//mpto\`\n *Utilisation: //mpto | @lenomdumembre#0000 Le MP à envoyer*\n\n\`//play\`\n *Utilisation: //play URL_de_la_musique*\n\n\`//pause\`\n *Utilisation: //pause*\n\n\`//resume\`\n *Utilisation: //resume*`
+			description: `**Liste des commandes :**\n\n\`//help\`\n *Utilisation: //help*\n\n\`//ping\`\n *Utilisation: //ping*\n\n\`//say\`\n *Utilisation: //say La chose à faire dire au bot !*\n\n\`//poll\`\n *Utilisation : //poll |Titre du sondage|Proposition 1|Proposition 2|Proposition 3|Proposition 4*\n\n\`//kick\`\n *Utilisation: //kick @lenomdumembre#0000 La raison du kick !*\n\n\`//ban\`\n *Utilisation: //ban @lenomdumembre#0000 La raison du ban !*\n\n\`//nuke\`\n *Utilisation: //nuke Un_nombre_entre_2_et_100*\n\n\`//mpto\`\n *Utilisation: //mpto | @lenomdumembre#0000 Le MP à envoyer*\n\n\`//play\`\n *Utilisation: //play URL_de_la_musique*\n\n\`//pause\`\n *Utilisation: //pause*\n\n\`//resume\`\n *Utilisation: //resume*\n\n\`//connect\`\n *Utilisation: //connect*\n\n\`//disconnect\`\n *Utilisation: //disconnect*`
 		}});
 	}
  
@@ -146,6 +146,7 @@ client.on("message", async message => {
 	}
 
 	if(command === "poll") {
+		message.delete();		
 		if(!message.member.roles.some(r=>["Staff"].includes(r.name)) )
 		return message.reply({embed: {
 			color: 15700514,
@@ -249,13 +250,28 @@ client.on("message", async message => {
 			}});
 	}
 	
-	if(commande === "pause") {
+	if(command === "pause") {
 		if(dispatcher !== undefined)
 			dispatcher.pause();
 	}
 		
-	if(commande === "resume") {
+	if(command === "resume") {
 		if(dispatcher !== undefined)
 			dispatcher.resume();
+	}
+	
+	if(command === "connect") {
+		if(message.member.voiceChannel) {
+			message.member.voiceChannel.join();
+		}
+		else
+			return message.reply({embed: {
+				color: 15700514,
+				description: "Allez dans un salon vocal !"
+			}});
+	}
+	
+	if(command === "disconnect") {
+		message.member.voiceChannel.leave();
 	}
 });
