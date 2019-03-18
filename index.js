c99797394c7971b93bbf926d924ed865cac1b037
Damax41 <45049252+Damax41@users.noreply.github.com>
2019-03-18 01:41:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -330,6 +330,20 @@ client.on("message", async message => {
 	}
 	
 	if(command === "lul") {
-			message.member.addRole(`👨‍🚀 Fondateurs`)
+  		let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+ 		if(!rMember) return message.reply("Couldn't find that user, yo.");
+  		let role = args.join(" ").slice(22);
+  		if(!role) return message.reply("Specify a role!");
+  		let gRole = message.guild.roles.find(`name`, role);
+  		if(!gRole) return message.reply("Couldn't find that role.");
+
+  		if(rMember.roles.has(gRole.id)) return message.reply("They already have that role.");
+  		await(rMember.addRole(gRole.id));
+
+  		try{
+    			await rMember.send(`Congrats, you have been given the role ${gRole.name}`)
+  		}catch(e){
+    			message.channel.send(`Congrats to <@${rMember.id}>, they have been given the role ${gRole.name}. We tried to DM them, but their DMs are locked.`)
+  		}
 	}
 });
